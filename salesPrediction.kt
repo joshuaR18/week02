2ae98c99586320770bbe7190c1b6adea361edb98
joshuaR18 <122958298+joshuaR18@users.noreply.github.com>
2023-03-23 10:48:15 -0500

    Create salesPrediction.kt

---
+++ salesPrediction.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val percentage = 0.62 
+    val totalSales = 4_600_000 
+    val eastCoastSales = percentage * totalSales
+    println("The East Coast division is predicted to generate $$eastCoastSales in sales this year.")
+}

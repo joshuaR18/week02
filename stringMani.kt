b29a9f2b44246e2e60b82460f99635f068552810
joshuaR18 <122958298+joshuaR18@users.noreply.github.com>
2023-03-23 12:19:22 -0500

    Create stringMani.kt

---
+++ stringMani.kt
@@ -0,0 +1,11 @@
+//joshua ramirez 3/23/2023
+print("Enter the name of your favorite city: ")
+val city = input.nextLine()
+
+println("Number of characters in the city name: ${city.length}")
+
+println("City name in uppercase: ${city.toUpperCase()}")
+
+println("City name in lowercase: ${city.toLowerCase()}")
+
+println("First character in the city name: ${city[0]}")

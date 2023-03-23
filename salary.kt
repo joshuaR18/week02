7315423e0f8aad7c3d3099c74aee63ace6d8a6fb
joshuaR18 <122958298+joshuaR18@users.noreply.github.com>
2023-03-23 09:37:33 -0500

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val name: String = "Joe Mahoney"
+    val age: Int = 26
+    val annualPay: Double = 100000.0
+
+    println("My name is $name, my age is $age and")
+    println("I hope to earn $$annualPay per year.")
+}
